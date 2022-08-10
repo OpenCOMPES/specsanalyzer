1318edf471ed587c1929186669e1d64d1c1f2840
rettigl <l.rettig@web.de>
2022-08-10 11:55:23 +0200

    default location for calib2d file and fixed tests, linting

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -10,8 +10,12 @@ from specsanalyzer.settings import parse_config
 
 package_dir = os.path.dirname(specsanalyzer.__file__)
 default_config_keys = [
-    'calib2d_file', 'nx_pixel', 'ny_pixel',
-    'pixel_size', 'magnification', 'apply_fft_filter',
+    "calib2d_file",
+    "nx_pixel",
+    "ny_pixel",
+    "pixel_size",
+    "magnification",
+    "apply_fft_filter",
 ]
 
 
@@ -23,17 +27,21 @@ def test_default_config():
 
 
 def test_load_dict():
-    config_dict = {'test_entry': True}
+    config_dict = {"test_entry": True}
     config = parse_config(config_dict)
     assert isinstance(config, dict)
     for key in default_config_keys:
         assert key in config.keys()
-    assert config['test_entry'] == True
+    assert config["test_entry"] is True
 
 
 def test_load_config():
-    config_json = load_config(Path(f"{package_dir}/../tests/data/config/config.json"))
-    config_yaml = load_config(Path(f"{package_dir}/../tests/data/config/config.yaml"))
+    config_json = load_config(
+        Path(f"{package_dir}/../tests/data/config/config.json"),
+    )
+    config_yaml = load_config(
+        Path(f"{package_dir}/../tests/data/config/config.yaml"),
+    )
     assert config_json == config_yaml
 
 
@@ -43,12 +51,12 @@ def test_load_config_raise():
 
 
 def test_insert_default_config():
-    dict1 = {'key1': 1, 'key2': 2, 'nesteddict': {'key4': 4}}
-    dict2 = {'key1': 2, 'key3': 3, 'nesteddict': {'key5': 5}}
+    dict1 = {"key1": 1, "key2": 2, "nesteddict": {"key4": 4}}
+    dict2 = {"key1": 2, "key3": 3, "nesteddict": {"key5": 5}}
     dict3 = insert_default_config(config=dict1, default_config=dict2)
     assert isinstance(dict3, dict)
-    for key in ['key1', 'key2', 'key3', 'nesteddict']:
+    for key in ["key1", "key2", "key3", "nesteddict"]:
         assert key in dict3.keys()
-    for key in ['key4', 'key5']:
-        assert key in dict3['nesteddict'].keys()
-    assert dict3['key1'] == 1
+    for key in ["key4", "key5"]:
+        assert key in dict3["nesteddict"].keys()
+    assert dict3["key1"] == 1
--- tests/test_specsanalyzer.py
+++ tests/test_specsanalyzer.py
@@ -6,10 +6,12 @@ from specsanalyzer import SpecsAnalyzer
 
 package_dir = os.path.dirname(specsanalyzer.__file__)
 
+
 def test_version():
     assert __version__ == "0.1.0"
 
+
 def test_default_config():
-    spa = SpecsAnalyzer(config={'calib2d_file' : f"{package_dir}/../tests/data/config/phoibos150.calib2d"})
+    spa = SpecsAnalyzer()
     assert isinstance(spa._config, dict)
-    assert spa._config['apply_fft_filter'] == False
+    assert spa._config["apply_fft_filter"] is False
