98fcb87e8fd7bf4ea013d62161df60a5032220b9
rettigl <l.rettig@web.de>
2022-08-10 12:06:27 +0200

    relative paths to package dir in tests and linting

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,47 +1,55 @@
+import os
 from pathlib import Path
 
 import pytest
 
+import specsscan
 from specsscan.settings import insert_default_config
 from specsscan.settings import load_config
 from specsscan.settings import parse_config
 
-default_config_keys = ['spa_params', 'data_path']
-default_spa_config_keys = ['apply_fft_filter']
+package_dir = os.path.dirname(specsscan.__file__)
+default_config_keys = ["spa_params", "data_path"]
+default_spa_config_keys = ["apply_fft_filter"]
+
 
 def test_default_config():
     config = parse_config()
     assert isinstance(config, dict)
     for key in default_config_keys:
         assert key in config.keys()
-    assert isinstance(config['spa_params'], dict)
+    assert isinstance(config["spa_params"], dict)
     for key in default_spa_config_keys:
-        assert key in config['spa_params'].keys()
+        assert key in config["spa_params"].keys()
+
 
 def test_load_dict():
-    config_dict = {'test_entry': True}
+    config_dict = {"test_entry": True}
     config = parse_config(config_dict)
     assert isinstance(config, dict)
     for key in default_config_keys:
         assert key in config.keys()
-    assert config['test_entry'] == True
+    assert config["test_entry"] is True
+
 
 def test_load_config():
-    config_json = load_config(Path("specsscan/tests/data/config.json"))
-    config_yaml = load_config(Path("specsscan/tests/data/config.yaml"))
+    config_json = load_config(Path(f"{package_dir}/../tests/data/config.json"))
+    config_yaml = load_config(Path(f"{package_dir}/../tests/data/config.yaml"))
     assert config_json == config_yaml
+
 
 def test_load_config_raise():
     with pytest.raises(TypeError):
         load_config("specsscan/requirements.txt")
 
+
 def test_insert_default_config():
-    dict1 = {'key1': 1, 'key2': 2, 'nesteddict':{'key4': 4}}
-    dict2 = {'key1': 2, 'key3': 3, 'nesteddict':{'key5': 5}}
-    dict3=insert_default_config(config=dict1, default_config=dict2)
+    dict1 = {"key1": 1, "key2": 2, "nesteddict": {"key4": 4}}
+    dict2 = {"key1": 2, "key3": 3, "nesteddict": {"key5": 5}}
+    dict3 = insert_default_config(config=dict1, default_config=dict2)
     assert isinstance(dict3, dict)
-    for key in ['key1', 'key2', 'key3', 'nesteddict']:
+    for key in ["key1", "key2", "key3", "nesteddict"]:
         assert key in dict3.keys()
-    for key in ['key4', 'key5']:
-        assert key in dict3['nesteddict'].keys()
-    assert dict3['key1'] == 1
+    for key in ["key4", "key5"]:
+        assert key in dict3["nesteddict"].keys()
+    assert dict3["key1"] == 1
--- tests/test_specsscan.py
+++ tests/test_specsscan.py
@@ -1,18 +1,13 @@
-import os
-
-import specsanalyzer
-
 from specsscan import __version__
 from specsscan import SpecsScan
 
-package_dir = os.path.dirname(specsanalyzer.__file__)
 
 def test_version():
     assert __version__ == "0.1.0"
 
 
 def test_default_config():
-    sps = SpecsScan(config={'spa_params':{'calib2d_file' : f"{package_dir}/../tests/data/config/phoibos150.calib2d"}})
+    sps = SpecsScan()
     assert isinstance(sps._config, dict)
-    assert 'spa_params' in sps._config.keys()
-    assert sps._config['spa_params']['apply_fft_filter'] == False
+    assert "spa_params" in sps._config.keys()
+    assert sps._config["spa_params"]["apply_fft_filter"] is False
