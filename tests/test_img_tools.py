aaae58b1f7607c12b032dcf9d0b56e914670db1c
rettigl <l.rettig@web.de>
2022-08-12 10:40:36 +0200

    more linting

--- tests/test_img_tools.py
+++ tests/test_img_tools.py
@@ -7,11 +7,15 @@ array2d = np.random.normal(size=bins2d)
 # strip negative values
 for i in range(0, array2d.shape[0]):
     for j in range(0, array2d.shape[1]):
-        array2d[i,j] = array2d[i][j] if array2d[i][j] > 0 else 0
+        array2d[i, j] = array2d[i][j] if array2d[i][j] > 0 else 0
 
 
 def test_fourier_filter_2d():
-    np.testing.assert_allclose(array2d, fourier_filter_2d(array2d, []), atol=1e-10)
+    np.testing.assert_allclose(
+        array2d,
+        fourier_filter_2d(array2d, []),
+        atol=1e-10,
+    )
 
 
 def test_fourier_filter_2d_raises():
