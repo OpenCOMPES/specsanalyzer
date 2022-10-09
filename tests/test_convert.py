2fc5844e34fa96d9c14685ceb4121cd5e2fd2503
puppo85 <33322314+puppo85@users.noreply.github.com>
2022-10-09 22:54:04 +0200

    pylint disable unuesed

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -55,7 +55,7 @@ def test_da_matrix():  # pylint: disable=too-many-locals
 
     # get the matrix_correction
     e_shift = np.array(config_dict["calib2d_dict"]["eShift"])
-    a_inner, da_matrix = get_damatrix_fromcalib2d(
+    a_inner, da_matrix = get_damatrix_fromcalib2d(   # pylint: disable=W0612
         lens_mode,
         kinetic_energy,
         pass_energy,
